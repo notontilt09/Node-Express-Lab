4090b59abda6c7291e27038f9dd836ee49fbf561
Daniel Weinman <namniewd@gmail.com>
2019-02-12 16:34:46 -0500

    wrote endpoint to POST new post to db

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -31,4 +31,22 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const newPost = req.body;
+    if (!newPost.title || !newPost.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+        Posts.insert(newPost)
+            .then(post => {
+                Posts.findById(post.id)
+                    .then(post => {
+                        res.status(201).json(post)
+                    })
+            })
+            .catch(err => {
+                res.status(500).json({ error: "There was an error while saving the post to the database" });
+            })
+    }  
+})
+
 module.exports = router;
