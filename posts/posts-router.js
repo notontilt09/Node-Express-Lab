5a0437e0a39776002c6a6d5b679e885da148c93c
Daniel Weinman <namniewd@gmail.com>
2019-02-12 16:25:47 -0500

    wrote endpoint to GET post by specific id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -15,4 +15,20 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Posts.findById(id)
+        .then(post => {
+            if (post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            } else {
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
+})
+
 module.exports = router;
